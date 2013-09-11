e28e5ea29ddc186d06277214fc3a8bc4740702b0
jinankjain3 <jinank.rocks@gmail.com>
2013-09-11 09:21:31 +0530

    Admin side completed

--- admin.php
+++ admin.php
@@ -1,21 +1,29 @@
-<?php session_start();
+<?php session_start(); ?>
+<html>
+<head>
+    </head>
+    <body>
+    <h2>This is admin side of crypto</h2>
+    <form method="POST" action="admin.php">
+        Enter level number = <input type="text" name="level_no"><br>
+        Enter level name = <input type="text" name="level_name"><br>
+        Enter solution = <input type="text" name="answer"><br>
+        <input type="submit" value="submit">
+    </form>
+</body>
+</html>
+<?php 
 include 'connect.php';
 if(isset($_SESSION['MEMBER_USERNAME'])&&$_SESSION['MEMBER_USERNAME']=="admin")
 {    
-    ?>
-    <h2>This is admin side of crypto</h2>
-    <form method="POST" action="admin.php">
-    	Enter level number = <input type="text" name="level_no">
-    	Enter level name = <input type="text" name="level_name">
-        Enter solution = <input type="text" name="answer">
-        <input type="submit" value="submit">
-    </form>
-    <?php
-    $_POST['level_no'] = $level_no;
-    $_POST['level_name'] = $level_name;
-    $_POST['answer'] = $answer;
-    $query_for_taking_username = "insert into answer (sno,answer,level,level_name) values ('','$answer','$level_no','$level_name')";
-    $query_run = mysql_query($query_for_taking_username);
+   $level_no = $_POST['level_no'] ;
+   $level_name = $_POST['level_name'];
+   $answer = $_POST['answer'];
+   if(isset($level_no)){
+    $query_for_taking_answers = "insert into answer (sno,answer,level,level_name) values ('','$answer','$level_no','$level_name')";
+    $query_run = mysql_query($query_for_taking_answers);
+}
+
 }else 
 header("location:index.php");
 ?>
