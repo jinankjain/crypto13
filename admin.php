1490c60487d9e392f3d1126a73e48c13087a21e9
jinankjain3 <jinank.rocks@gmail.com>
2013-09-12 09:13:00 +0530

    new look

--- admin.php
+++ admin.php
@@ -16,9 +16,9 @@
 include 'connect.php';
 if(isset($_SESSION['MEMBER_USERNAME'])&&$_SESSION['MEMBER_USERNAME']=="admin")
 {    
-   $level_no = $_POST['level_no'] ;
-   $level_name = $_POST['level_name'];
-   $answer = $_POST['answer'];
+   $level_no = @$_POST['level_no'] ;
+   $level_name = @$_POST['level_name'];
+   $answer = @$_POST['answer'];
    if(isset($level_no)){
     $query_for_taking_answers = "insert into answer (sno,answer,level,level_name) values ('','$answer','$level_no','$level_name')";
     $query_run = mysql_query($query_for_taking_answers);
--- ques1.php
+++ ques1.php
@@ -7,4 +7,60 @@ if(isset($_SESSION['MEMBER_USERNAME']))
 header("location:index.php");
 
 ?>
+<<<<<<< HEAD
 <a href="logout.php">Logout</a>
+=======
+<html>
+	<head>
+		<title>Crypto 13</title>
+		<meta charset="utf-8">
+		
+		<script type="text/javascript" src="js/jquery-10.js"></script>
+		<script type="text/javascript" src="js/jquery-migrate-1.2.1.min.js"></script>
+
+		<style type="text/css">
+		body
+		{
+			overflow: hidden;
+			background-color: #000;
+		}
+		#base
+		{
+			position:fixed;
+			top:0;
+			left:0;
+			background-color:black;
+		}
+		#base img.fade
+		{
+			position:absolute;
+			top:0;
+			display:none;
+			width:100%;
+			height:100%;
+			z-index:-10;
+		}
+		</style>
+
+			<script type="text/javascript">
+				$(window).load(function(){
+					var dg_H = $(window).height();
+					var dg_W = $(window).width();
+					$('#base').css({'height':dg_H/3,'width':dg_W/4, 'top':2*dg_H/3, 'left':3*dg_W/8});
+					function appear() {
+						$("#base img").first().fadeIn(4000);
+					}
+					appear();
+				})
+				$(window).resize(function(){window.location.href=window.location.href})
+			</script>
+			
+	</head>
+	<body>
+		
+		<div id="base">
+			<img class="fade" src="images/candle.gif">
+		</div>	
+	</body>
+</html>
+>>>>>>> 266ce1e9146d11445949a6c207efdd5730bbbc64
